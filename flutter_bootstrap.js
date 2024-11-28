bb807ab92ec1f9a0e90477d9157b4f5b1d04e970
markcabalona <work@markcabalona.dev>
2024-11-28 16:56:30 +0800

    feature: update base href tag to <base href="/townsquare-demo>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3197529403"
+    serviceWorkerVersion: "2827690012"
   }
 });
